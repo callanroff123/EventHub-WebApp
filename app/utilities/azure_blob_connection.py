ec25594cea2161f14ebae0965bc85475d9ba875d
callanroff123 <callanroff@gmail.com>
2025-04-03 23:58:10 +1100

    temp testing connection string

--- app/utilities/azure_blob_connection.py
+++ app/utilities/azure_blob_connection.py
@@ -14,6 +14,7 @@ def read_from_azure_blob_storage(connection_string, container_name, file_name):
             * container_name (str): The container we wish to connect to/create and connect to wrt the corresponding connection string's account
             * file_name (str): The name of the location (file) in the container where the read will occur
     '''
+    print(f"connection_string is: {connection_string}")
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
     container_client = blob_service_client.get_container_client(container_name)
     blob_client = container_client.get_blob_client(file_name)
